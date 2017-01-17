2fe32528a4d77d8e317630e0d7d6df4a333bd332
zoox101 <william@thebookers.net>
2017-01-17 16:01:50 -0600

    Reorganized Project

--- SpeciationSimulation/src/NewSim/AAADriver.java
+++ SpeciationSimulation/src/NewSim/AAADriver.java
@@ -5,7 +5,6 @@ import java.io.IOException;
  * @author William Booker
  * Based on work by Kevin and Mark
  * Speciation Simulation v.2.0 - 10/12/16
- * For use in Dr. Hougen's REAL Lab
  */
 public class AAADriver {
 	
